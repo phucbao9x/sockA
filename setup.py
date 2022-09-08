3d1b3749c921f65af51e63f679bd507307cb7fd6
phucbao9x <nguyentranphucbao.working@gmail.com>
2022-09-08 08:17:49 +0700

    C2-sockA, gitignore, setup, README, LICENSE

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='sockA',
+    version='0.0.1',
+    description='SockA supported socket server!',
+    url='https://www.github.com/phucbao9x/sockA.git',
+    package=['sockA'],
+    classifiers=[
+        'Progamming Language :: Python :: 3',
+        'Progamming Language :: Python :: 3.10.x'
+    ],
+    include_package_data=True
+)
